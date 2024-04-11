70badeb5b89b82821cb1a85b3b9ddfa9de79d4f0
Savitha Shree <157434708+savithashreem07@users.noreply.github.com>
2024-04-10 21:10:19 -0700

    Update chat_app.py

--- Customer Support System/chat_app.py
+++ Customer Support System/chat_app.py
@@ -1,16 +1,14 @@
-from langchain_openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import DocArrayInMemorySearch
-from langchain_community.document_loaders import TextLoader
-from langchain.chains import RetrievalQA,  ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
-from langchain_openai import ChatOpenAI
-from langchain_community.document_loaders import PyPDFLoader
-import panel as pn
-import param
 import os
 import warnings
 from openai import OpenAI
+from langchain_community.vectorstores import DocArrayInMemorySearch
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.chains import ConversationalRetrievalChain
 
 from flask import Flask, redirect, render_template, request
 
@@ -30,153 +28,98 @@ os.environ["TAVILY_API_KEY"] = os.environ.get('OPENAI_API_KEY')
 
 warnings.filterwarnings("ignore")
 
+delimiter = "####"
 
-def load_db(file, chain_type, k):
-    # load documents
-    loader = PyPDFLoader(file)
-    documents = loader.load()
-    # split documents
-    text_splitter = RecursiveCharacterTextSplitter(
-           chunk_size=1000, 
-           chunk_overlap=150)
-    docs1 = text_splitter.split_documents(documents)
-    # define embedding
-    embeddings = OpenAIEmbeddings()
-    # create vector database from data
-    db = DocArrayInMemorySearch.from_documents(docs1, 
-           embeddings)
-    # define retriever
-    retriever = db.as_retriever(search_type="similarity", 
-           search_kwargs={"k": k})
-    # create a chatbot chain. Memory is managed externally.
-    qa = ConversationalRetrievalChain.from_llm(
-        llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0), 
-        chain_type=chain_type, 
-        retriever=retriever, 
-        return_source_documents=True,
-        return_generated_question=True,
+# Load the PDF file
+loader = PyPDFLoader("docs/2023Catalog.pdf")
+documents = loader.load()
+
+# split documents
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+docs1 = text_splitter.split_documents(documents)
+
+embeddings = OpenAIEmbeddings()
+vectordb = DocArrayInMemorySearch.from_documents(docs1, embeddings)
+
+memory = ConversationBufferWindowMemory(
+    memory_key="chat_history",
+    input_key='question',
+    output_key='answer',
+    return_messages=True,
+    k=5
+)
+
+def get_completion_from_messages(messages, 
+                                 model="gpt-3.5-turbo", 
+                                 temperature=0, 
+                                 max_tokens=500):
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, 
+        max_tokens=max_tokens, 
     )
-    return qa 
+    return response.choices[0].message.content
 
-class cbfs(param.Parameterized):
-    chat_history = param.List([])
-    answer = param.String("")
-    db_query  = param.String("")
-    db_response = param.List([])
+def get_translation(text, language):
+    system_message = f"""Assume you are a professional translator.\
+    The text will be delimited by tripe backticks.\
+    {delimiter}{text}{delimiter}"""
 
-    def __init__(self,  **params):
-        super(cbfs, self).__init__( **params)
-        self.panels = []
-        self.loaded_file = "docs/MachineLearning-Lecture01.pdf"
-        self.qa = load_db(self.loaded_file,"stuff", 4)
+    user_message = f"""Translate the text into {language} language."""
 
-    def call_load_db(self, count):
-        # init or no file specified :
-        if count == 0 or file_input.value is None:  
-            return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
-        else:
-            file_input.save("temp.pdf")  # local copy
-            self.loaded_file = file_input.filename
-            button_load.button_style="outline"
-            self.qa = load_db("temp.pdf", "stuff", 4)
-            button_load.button_style="solid"
-        self.clr_history()
-        return pn.pane.Markdown(f"Loaded File: {self.loaded_file}")
+    messages =  [  
+    {'role':'system', 
+    'content': system_message},    
+    {'role':'user', 
+    'content': f"{delimiter}{user_message}{delimiter}"},  
+    ]
 
-    def convchain(self, query):
-        if not query:
-            return pn.WidgetBox(pn.Row('User:', 
-               pn.pane.Markdown("", width=600)), scroll=True)
-        result = self.qa({"question": query, 
-                          "chat_history": self.chat_history})
-        self.chat_history.extend([(query, result["answer"])])
-        self.db_query = result["generated_question"]
-        self.db_response = result["source_documents"]
-        self.answer = result['answer'] 
-        self.panels.extend([
-            pn.Row('User:', pn.pane.Markdown(query, width=600)),
-            pn.Row('ChatBot:', pn.pane.Markdown(self.answer, 
-               width=600, 
-               style={'background-color': '#F6F6F6'}))
-        ])
-        inp.value = ''  # clears loading indicator when cleared
-        return pn.WidgetBox(*self.panels, scroll=True)
-    
-    @param.depends('db_query')
-    def display_db_query(self):
-        if not self.db_query:
-            return pn.Column(
-                pn.Row(pn.pane.Markdown(f"Last question to DB:", 
-            styles={'background-color': '#F6F6F6'})),
-                pn.Row(pn.pane.Str("no DB accesses so far"))
-            )
-        return pn.Column(pn.Row(pn.pane.Markdown(f"DB query:", styles={'background-color': '#F6F6F6'})), pn.pane.Str(self.db_query))
+    translate = get_completion_from_messages(messages)
+    print(translate + "\n")
+    return translate
 
-    # @param.depends('db_query ', )
-    # def convchain(self):
-    #     if not self.db_query :
-    #         return pn.Column(
-    #             pn.Row(pn.pane.Markdown(f"Last question to DB:", 
-    #         styles={'background-color': '#F6F6F6'})),
-    #             pn.Row(pn.pane.Str("no DB accesses so far"))
-    #         )
-    #     return pn.Column(pn.Row(pn.pane.Markdown(f"DB query:", styles={'background-color': '#F6F6F6'})),pn.pane.Str(self.db_query ))
-    
-    @param.depends('db_response', )
-    def get_sources(self):
-        if not self.db_response:
-            return pn.WidgetBox(pn.Row(pn.pane.Str("No DB response yet")), width=600, scroll=True)
-        rlist=[pn.Row(pn.pane.Markdown(f"Result of DB lookup:", 
-            styles={'background-color': '#F6F6F6'}))]
-        for doc in self.db_response:
-            rlist.append(pn.Row(pn.pane.Str(doc)))
-        return pn.WidgetBox(*rlist, width=600, scroll=True)
+def generate_response(memory, question):
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
-    @param.depends('convchain', 'clr_history') 
-    def get_chats(self):
-        if not self.chat_history:
-            return pn.WidgetBox(
-                  pn.Row(pn.pane.Str("No History Yet")), 
-                   width=600, scroll=True)
-        rlist=[pn.Row(pn.pane.Markdown(
-            f"Current Chat History variable", 
-            styles={'background-color': '#F6F6F6'}))]
-        for exchange in self.chat_history:
-            rlist.append(pn.Row(pn.pane.Str(exchange)))
-        return pn.WidgetBox(*rlist, width=600, scroll=True)
+    template = """
+    Assume you are San Francisco Bay University Assistant. Your task it to answer questions about the catalog. Use the following pieces of \
+    context to answer the question at the end. \
+    If you don't know the answer, \
+    just say that you don't know, don't try \
+    to make up an answer.
+    {context}
+    Question: {question}
+    Helpful Answer:"""
 
-    def clr_history(self,count=0):
-            self.chat_history = []
-            return
-    
-chatbot = cbfs()
-# class ChatApp(param.Parameterized):
-#     loaded_file = "docs/MachineLearning-Lecture01.pdf"
-#     chatbot = cbfs()
+    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],template=template)
+    qa_chain = ConversationalRetrievalChain.from_llm(
+        llm,
+        retriever=vectordb.as_retriever(),
+        return_source_documents=True,
+        combine_docs_chain_kwargs={"prompt": QA_CHAIN_PROMPT},
+        get_chat_history=lambda h : h,
+        memory=memory,
+    )
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    language = 'en'
-    answer = ''
-    question = ''
+    return qa_chain.invoke(input={"question": question})
 
-    if request.method == 'POST':
-        query = request.form['query']
-        chatbot.convchain(query)
-        return render_template('index.html', answer=chatbot.answer, db_query=chatbot.db_query, db_response=chatbot.db_response)
-    
-@app.route('/')
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    # template = pn.template.FastListTemplate(
-    #     site="Chatbot UI",
-    #     title="Chatbot Interface",
-    #     header_background="#F6F6F6",
-    #     main=[pn.panel(chatbot.param, show_name=False)],
-    #     main_max_width="768px",
-    # )
-    # template.theme(pn.template.DefaultTheme)
-    # return template.servable()
-    return render_template('index.html', answer="", db_query="", db_response=[])
+    result = ''
+    language = request.form.get('language')
+    question = request.form.get('question')
+    translate = request.form.get('translate') == True
+
+    result = generate_response(memory, question)['answer']
+
+    if translate:
+        result = get_translation(result, language)
+
+    print(result)
+
+    return render_template('index.html', result = result, language = language)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
